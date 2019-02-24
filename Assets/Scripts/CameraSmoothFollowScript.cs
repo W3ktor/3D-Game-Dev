142ede59f3f8b1648893eedbeac2b83a3dd2adab
Bence Konecsny <bence.konecsny@ecomxperts.de>
2019-02-24 23:58:05 +0100

    Create script for camera to follow target smoothly

---
+++ Assets/Scripts/CameraSmoothFollowScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSmoothFollowScript : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 targetOffset;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.transform.position + targetOffset;
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
